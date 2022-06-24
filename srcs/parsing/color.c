e895843df9297bfd92fbf4721d2379d0ab90da90
Nicolas BURAT DE GURGY <nburat-d@e3r3p12.clusters.42paris.fr>
2022-06-24 16:26:40 +0200

    NORMINETTE

--- srcs/parsing/color.c
+++ srcs/parsing/color.c
@@ -6,7 +6,7 @@
 /*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 13:27:42 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/06/24 16:19:20 by nburat-d         ###   ########.fr       */
+/*   Updated: 2022/06/24 16:26:23 by nburat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,11 @@ int	is_rgb_format(char *str_color)
 	i = 0;
 	splitted = ft_split(str_color, ',');
 	if (ft_tablen(splitted) != 3)
-		return(free_tab(splitted), FALSE);
-	while(splitted[i])
+		return (free_tab(splitted), FALSE);
+	while (splitted[i])
 	{
-		if(ft_is_int(splitted[i]) && ft_atoi(splitted[i]) <= 255 && ft_atoi(splitted[i]) >= 0)
+		if (ft_is_int(splitted[i])
+			&& ft_atoi(splitted[i]) <= 255 && ft_atoi(splitted[i]) >= 0)
 			i++;
 		else
 			return (free_tab(splitted), FALSE);
@@ -34,34 +35,34 @@ int	is_rgb_format(char *str_color)
 int	assign_color(char *str_color, t_color *color)
 {	
 	char	**rgb_tab;
-	
+
 	if (!is_rgb_format(str_color))
 	{
-		ft_putstr_fd(ERROR_COLOR_FORMAT, 2);		
+		ft_putstr_fd(ERROR_COLOR_FORMAT, 2);
 		return (-1);
 	}
 	rgb_tab = ft_split(str_color, ',');
 	color->red = ft_atoi(rgb_tab[0]);
 	color->green = ft_atoi(rgb_tab[1]);
 	color->blue = ft_atoi(rgb_tab[2]);
-	return(free_tab(rgb_tab), 0);
+	return (free_tab(rgb_tab), 0);
 }
 
 int	set_color(t_data *data)
 {
-	if(!data)
-		return(-1);
+	if (!data)
+		return (-1);
 	data->floor_color = ft_calloc(1, sizeof(t_color));
-	if(!data->floor_color)
-		return(ft_putstr_fd(ERROR_MALLOC, 2), -1);
+	if (!data->floor_color)
+		return (ft_putstr_fd(ERROR_MALLOC, 2), -1);
 	if (assign_color(data->texture->floor, data->floor_color) != 0)
 	{
 		free(data->floor_color);
 		return (-1);
 	}
 	data->ceiling_color = ft_calloc(1, sizeof(t_color));
-	if(!data->ceiling_color)
-		return(ft_putstr_fd(ERROR_MALLOC, 2), -1);
+	if (!data->ceiling_color)
+		return (ft_putstr_fd(ERROR_MALLOC, 2), -1);
 	if (assign_color(data->texture->ceiling, data->ceiling_color) != 0)
 	{
 		free(data->floor_color);
