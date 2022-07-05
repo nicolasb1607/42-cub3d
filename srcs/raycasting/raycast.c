7a241db892176395552390c750e76a9ded819394
Rodolphe Pottier <rodolphegamer@hotmail.fr>
2022-07-05 10:17:08 +0200

    WIP petite réorga fichiers

--- srcs/raycasting/raycast.c
+++ srcs/raycasting/raycast.c
@@ -6,7 +6,7 @@
 /*   By: rpottier <rpottier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 16:02:56 by rpottier          #+#    #+#             */
-/*   Updated: 2022/07/04 17:14:27 by rpottier         ###   ########.fr       */
+/*   Updated: 2022/07/05 10:16:46 by rpottier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ t_list	*cast_all_ray(t_player *player, t_map *map)
 	double	ray_angle;
 
 	angle_incr = FOV / NUMBER_OF_RAYS;
+	printf("angle_incr = %f\n", angle_incr);
 	ray_angle = player->rotation_angle - (FOV / 2);
 	all_rays = NULL;
 	i = 0;
@@ -30,7 +31,6 @@ t_list	*cast_all_ray(t_player *player, t_map *map)
 		if (ray_angle < 0)
 			ray_angle += (2 * PI);
 		ray_angle = fmod(ray_angle, PI *2);
-		printf("%d.  %f\n", i, ray_angle);
 		ray = cast_ray(ray_angle, player, map);
 		ft_lstadd_back(&all_rays, ft_lstnew(ray));
 		ray_angle += angle_incr;
@@ -79,30 +79,6 @@ void	set_facing_values(t_ray *ray)
 		
 }
 
-int	adjust_coordonate(t_ray *ray, int coordonate, int hit_direction)
-{
-	if (hit_direction == HORIZONTAL)
-	{
-		if (ray->facing_up_down == UP)
-			return (coordonate - 1);
-		return (coordonate);
-	}
-	else
-	{
-		if (ray->facing_left_right == LEFT)
-			return (coordonate - 1);
-		return (coordonate);
-	}
-}
-
-int is_inside_map(int x, int y, t_map *map)
-{
-	if (x >= 0 && x < map->width * TILE_SIZE && y >= 0 && y < map->height * TILE_SIZE)
-		return (TRUE);
-	else
-		return (FALSE);
-}
-
 void	get_horizontal_hit(t_ray *ray, t_player *player, t_map *map)
 {
 
@@ -122,13 +98,13 @@ void	get_horizontal_hit(t_ray *ray, t_player *player, t_map *map)
 	ray->exist_horizontal_hit = FALSE;
 	while (ray->intersection.y >= 0 && ray->intersection.y < TILE_SIZE * map->height && ray->intersection.x >= 0 && ray->intersection.x < TILE_SIZE * map->width)
 	{
+		printf("in get_horizontal_hit\n");
 		printf("y = %f x = %f\n", ray->intersection.y, ray->intersection.x);
 		if (is_hiting_a_wall(map, ray->intersection.x, adjust_coordonate(ray,ray->intersection.y, HORIZONTAL)))
 		{
 			ray->horizontal_hit.x = ray->intersection.x;
 			ray->horizontal_hit.y = ray->intersection.y;
 			ray->exist_horizontal_hit = TRUE;
-			printf("TEMOIN\n");
 			break ;
 		}
 		else
@@ -169,6 +145,52 @@ void	get_vertical_hit(t_ray *ray, t_player *player, t_map *map)
 	}
 }
 
+void	get_shortest_distance(t_ray *ray, t_player *player)
+{
+	double	vertical_distance;
+	double	horizontal_distance;
+
+	vertical_distance = distance(player->x_pos, player->y_pos, ray->vertical_hit.x, ray->vertical_hit.y);
+	horizontal_distance = distance(player->x_pos, player->y_pos, ray->horizontal_hit.x, ray->horizontal_hit.y);
+	if (ray->exist_horizontal_hit && ray->exist_vertical_hit)
+	{
+//		printf("vertical_distance = %f, horizontal_distance %f\n", vertical_distance, horizontal_distance);
+		if (vertical_distance <= horizontal_distance)
+			update_closest_wall(ray, VERTICAL, vertical_distance);
+		else
+			update_closest_wall(ray, HORIZONTAL, horizontal_distance);
+	}
+	else if (ray->exist_horizontal_hit)
+		update_closest_wall(ray, HORIZONTAL, horizontal_distance);
+	else
+		update_closest_wall(ray, VERTICAL, vertical_distance);
+	return ;
+}
+
+int	adjust_coordonate(t_ray *ray, int coordonate, int hit_direction)
+{
+	if (hit_direction == HORIZONTAL)
+	{
+		if (ray->facing_up_down == UP)
+			return (coordonate - 1);
+		return (coordonate);
+	}
+	else
+	{
+		if (ray->facing_left_right == LEFT)
+			return (coordonate - 1);
+		return (coordonate);
+	}
+}
+
+int is_inside_map(int x, int y, t_map *map)
+{
+	if (x >= 0 && x < map->width * TILE_SIZE && y >= 0 && y < map->height * TILE_SIZE)
+		return (TRUE);
+	else
+		return (FALSE);
+}
+
 double	distance(double x1, double y1, double x2, double y2)
 {
 	return (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
@@ -189,113 +211,3 @@ void	update_closest_wall(t_ray *ray, int orientation, int distance)
 		ray->distance = distance;
 	}
 }
-
-void	get_shortest_distance(t_ray *ray, t_player *player)
-{
-	double	vertical_distance;
-	double	horizontal_distance;
-
-	vertical_distance = distance(player->x_pos, player->y_pos, ray->vertical_hit.x, ray->vertical_hit.y);
-	horizontal_distance = distance(player->x_pos, player->y_pos, ray->horizontal_hit.x, ray->horizontal_hit.y);
-	if (ray->exist_horizontal_hit && ray->exist_vertical_hit)
-	{
-		printf("vertical_distance = %f, horizontal_distance %f\n", vertical_distance, horizontal_distance);
-		if (vertical_distance <= horizontal_distance)
-			update_closest_wall(ray, VERTICAL, vertical_distance);
-		else
-			update_closest_wall(ray, HORIZONTAL, horizontal_distance);
-	}
-	else if (ray->exist_horizontal_hit)
-		update_closest_wall(ray, HORIZONTAL, horizontal_distance);
-	else
-		update_closest_wall(ray, VERTICAL, vertical_distance);
-	return ;
-}
-
-//######################################################################
-//#                               old version                          #
-//######################################################################
-
-/*
-
-void	get_horizontal_hit(t_ray *ray, t_player *player, t_map *map)
-{
-	t_2d next_intersection;
-	
-	ray->first_intersection.y = (int)((player->y_pos / TILE_SIZE) + ray->increment_top_down) * TILE_SIZE ;
-	ray->first_intersection.x = (int)player->x_pos + (((ray->first_intersection.y - player->y_pos) / tan(ray->rad_angle)) * ray->facing_up_down);
-
-	if(is_inside_map(ray->first_intersection.x, ray->first_intersection.y, map) && is_hiting_a_wall(map,  ray->first_intersection.x, adjust_coordonate(ray, ray->first_intersection.y, HORIZONTAL)))
-	{
-		ray->horizontal_hit.x = ray->first_intersection.x;
-		ray->horizontal_hit.y = ray->first_intersection.y;
-		return ;
-	}
-	// CALCUL SECOND INTERSECTIONL
-
-	ray->second_intersection.y = ray->first_intersection.y + (TILE_SIZE * ray->facing_up_down);
-	ray->second_intersection.x = ray->first_intersection.x + (((ray->first_intersection.y - ray->second_intersection.y) / tan(ray->rad_angle)) * ray->facing_up_down);
-	if(is_inside_map(ray->second_intersection.x, ray->second_intersection.y, map) && is_hiting_a_wall(map, ray->second_intersection.x,adjust_coordonate(ray, ray->second_intersection.y, HORIZONTAL)))
-	{
-		ray->horizontal_hit.x = ray->second_intersection.x;
-		ray->horizontal_hit.y = ray->second_intersection.y;
-		return ;
-	}
-	// CALCUL STEPS
-	ray->y_step = TILE_SIZE * ray->facing_up_down;
-	ray->x_step = TILE_SIZE / tan(ray->rad_angle) * ray->facing_left_right;
-	next_intersection.x = ray->second_intersection.x * ray->x_step;
-	next_intersection.y = ray->second_intersection.y * ray->y_step;
-	while (is_inside_map(next_intersection.x, next_intersection.y, map))
-	{
-		if (is_hiting_a_wall(map, next_intersection.x, adjust_coordonate(ray, next_intersection.y, HORIZONTAL)))
-		{
-			ray->horizontal_hit.x = next_intersection.x;
-			ray->horizontal_hit.y = next_intersection.y;
-		}
-		else
-		{
-			next_intersection.x += ray->x_step;
-			next_intersection.y += ray->y_step;
-		}
-	}
-	return ;	
-}
-
-void	get_vertical_hit(t_ray *ray, t_player *player, t_map *map)
-{
-	t_2d next_intersection;
-
-	// CALCUL FIRST INTERSECTION
-	ray->first_intersection.x = (int)((player->x_pos / TILE_SIZE) + ray->increment_left_right) * TILE_SIZE;
-	ray->first_intersection.y = (int)player->y_pos + (((player->x_pos - ray->first_intersection.x) * tan(ray->rad_angle)) * ray->facing_left_right);
-	if(is_inside_map(ray->first_intersection.x, ray->first_intersection.y, map) && is_hiting_a_wall(map, adjust_coordonate(ray, ray->first_intersection.x, VERTICAL),ray->first_intersection.y))
-	{
-		ray->vertical_hit.x = ray->first_intersection.x;
-		ray->vertical_hit.y = ray->first_intersection.y;
-		return ;
-	}
-	// CALCUL SECOND INTERSECTION
-	ray->second_intersection.x +=  TILE_SIZE * ray->facing_left_right;
-	ray->second_intersection.y =  ray->first_intersection.y + (TILE_SIZE * tan(ray->rad_angle)) * ray->facing_up_down;
-	if(is_inside_map(ray->second_intersection.x, ray->second_intersection.y, map) && is_hiting_a_wall(map, adjust_coordonate(ray, ray->second_intersection.x, VERTICAL), ray->second_intersection.y))
-	{
-		ray->vertical_hit.x = ray->second_intersection.x;
-		ray->vertical_hit.y = ray->second_intersection.y;
-		return ;
-	}
-	// CALCUL STEPS
-	ray->y_step = (TILE_SIZE / tan(ray->rad_angle)) * ray->facing_up_down;
-	ray->x_step = TILE_SIZE * ray->facing_left_right;
-	next_intersection.x = ray->second_intersection.x * ray->x_step;
-	next_intersection.y = ray->second_intersection.y * ray->y_step;
-	while (is_inside_map(next_intersection.x, next_intersection.y, map) && !is_hiting_a_wall(map, adjust_coordonate(ray, next_intersection.x, VERTICAL), next_intersection.y))
-	{
-		next_intersection.x += ray->x_step;
-		next_intersection.y += ray->y_step;
-	}
-	ray->vertical_hit.x = next_intersection.x;
-	ray->vertical_hit.y = next_intersection.y;
-	return ;	
-}
-*/
