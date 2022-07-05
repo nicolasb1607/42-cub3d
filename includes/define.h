cb905b7003ed77eea29df5f97f9857be4dacb830
Rodolphe Pottier <rodolphegamer@hotmail.fr>
2022-07-05 12:29:44 +0200

    FIX Débordement du rendu du rayon sur les murs

--- includes/define.h
+++ includes/define.h
@@ -6,7 +6,7 @@
 /*   By: rpottier <rpottier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 10:29:29 by rpottier          #+#    #+#             */
-/*   Updated: 2022/07/04 17:08:08 by rpottier         ###   ########.fr       */
+/*   Updated: 2022/07/05 12:29:39 by rpottier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@
 
 # define WIDTH_RAY 4
 # define NUMBER_OF_RAYS (WIDTH_WIN / WIDTH_RAY)
-# define PROJECTION_PLANE ((WIDTH_WIN / 2) / tan(FOV / 2))
+# define PROJECTION_PLANE
 
 # define WALK_SPEED TILE_SIZE / 10
 # define ROTATION_SPEED 4 * (PI / 180)
