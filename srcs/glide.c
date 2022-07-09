4f608d80fdbaded670c1bd02481d307f08f65c29
Rodolphe Pottier <rodolphegamer@hotmail.fr>
2022-07-09 22:06:50 +0200

    refactor glide

--- srcs/glide.c
+++ srcs/glide.c
@@ -6,7 +6,7 @@
 /*   By: rpottier <rpottier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 16:16:12 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/07/09 22:04:43 by rpottier         ###   ########.fr       */
+/*   Updated: 2022/07/09 22:06:15 by rpottier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 void	glide(t_player *player, t_data *data, int new_x, int new_y)
 {
 	if (!player_is_hiting_a_wall(&data->map, new_x, player->y))
+	{
 		player->x = new_x;
+		return ;
+	}
 	if (!player_is_hiting_a_wall(&data->map, player->x, new_y))
 		player->y = new_y;
 }
