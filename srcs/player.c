4133174dc84ab33257bd18f7e6c01d3cd109149a
Rodolphe Pottier <rodolphegamer@hotmail.fr>
2022-07-11 08:57:47 +0200

    norming

--- srcs/player.c
+++ srcs/player.c
@@ -3,14 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   player.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rpottier <rpottier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 09:09:30 by rpottier          #+#    #+#             */
-/*   Updated: 2022/07/08 16:24:31 by nburat-d         ###   ########.fr       */
+/*   Updated: 2022/07/11 08:57:39 by rpottier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "player.h"
+
+void	update_player(t_data *data)
+{
+	int	move_step;
+	int	new_player_x;
+	int	new_player_y;
+
+	new_player_x = 0;
+	new_player_y = 0;
+	move_step = (data->player.walk_direction * data->player.walk_speed);
+	data->player.rotation_angle = data->player.rotation_angle
+		- (data->player.left_right_rotation * data->player.rotation_speed);
+	if (data->player.rotation_angle < 0)
+		data->player.rotation_angle += (2 * PI);
+	data->player.rotation_angle = fmod(data->player.rotation_angle, 2 * PI);
+	new_player_x = round(data->player.x + (cos(data->player.rotation_angle
+					+ data->player.side_move_angle) * move_step));
+	new_player_y = round(data->player.y + (sin(data->player.rotation_angle
+					+ data->player.side_move_angle) * move_step));
+	apply_new_coordonate(data, new_player_x, new_player_y);
+	data->player.left_right_rotation = 0;
+	data->player.walk_direction = 0;
+	data->player.side_move_angle = 0;
+}
+
+void	apply_new_coordonate(t_data *data, int new_player_x, int new_player_y)
+{
+	if (!player_is_hiting_a_wall(&data->map, new_player_x, new_player_y))
+	{
+		data->player.x = new_player_x;
+		data->player.y = new_player_y;
+	}
+	else if (data->player.walk_direction == 1
+		&& data->player.side_move_angle == 0)
+	{
+		if (glide_x(&data->player, data, new_player_x) == -1)
+			glide_y(&data->player, data, new_player_y);
+	}
+}
 
 int	player_is_hiting_a_wall(t_map *map, int x, int y)
 {
@@ -36,43 +75,4 @@ void	refactor_angle(double *angle)
 	if (*angle < 0)
 		*angle += (2 * PI);
 	*angle = fmod(*angle, PI * 2);
-}
-
-void	apply_new_coordonate(t_data *data, int new_player_x, int new_player_y)
-{
-	if (!player_is_hiting_a_wall(&data->map, new_player_x, new_player_y))
-	{
-		data->player.x = new_player_x;
-		data->player.y = new_player_y;
-	}
-	else if (data->player.walk_direction == 1
-		&& data->player.side_move_angle == 0)
-	{
-		if (glide_x(&data->player, data, new_player_x) == -1)
-			glide_y(&data->player, data, new_player_y);
-	}
-}
-
-void	update_player(t_data *data)
-{
-	int	move_step;
-	int	new_player_x;
-	int	new_player_y;
-
-	new_player_x = 0;
-	new_player_y = 0;
-	move_step = (data->player.walk_direction * data->player.walk_speed);
-	data->player.rotation_angle = data->player.rotation_angle
-		- (data->player.left_right_rotation * data->player.rotation_speed);
-	if (data->player.rotation_angle < 0)
-		data->player.rotation_angle += (2 * PI);
-	data->player.rotation_angle = fmod(data->player.rotation_angle, 2 * PI);
-	new_player_x = round(data->player.x + (cos(data->player.rotation_angle
-					+ data->player.side_move_angle) * move_step));
-	new_player_y = round(data->player.y + (sin(data->player.rotation_angle
-					+ data->player.side_move_angle) * move_step));
-	apply_new_coordonate(data, new_player_x, new_player_y);
-	data->player.left_right_rotation = 0;
-	data->player.walk_direction = 0;
-	data->player.side_move_angle = 0;
 }
